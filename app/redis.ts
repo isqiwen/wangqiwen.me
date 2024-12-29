2c359c7de488d97c499ebcdfd2b0b300c7ab0ec9
wangqiwen <isqiwen@gmail.com>
2024-12-29 18:56:06 +0800

    test

--- app/redis.ts
+++ app/redis.ts
@@ -1,12 +1,12 @@
 import { Redis } from "@upstash/redis";
 
 if (!process.env.UPSTASH_REDIS_REST_TOKEN) {
-  throw new Error("UPSTASH_REDIS_REST_TOKEN is not defined");
+    throw new Error("UPSTASH_REDIS_REST_TOKEN is not defined");
 }
 
 const redis = new Redis({
-  url: "https://distinct-glider-39002.upstash.io",
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+    url: "https://distinct-glider-39002.upstash.io",
+    token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
 
 export default redis;
